caabe5a69fa75a0ab393d05291051fb39607c199
Marcel <marcel@MBP-von-Marcel.fritz.box>
2025-11-05 21:02:33 +0100

    add src/main

---
+++ src/main/se1.play/application/package-info.java
@@ -0,0 +1,13 @@
+package application;
+
+/**
+ * The {@link application} package includes classes with a {@code main()} -
+ * function executable by the Java VM.
+ *
+ * @version <code style=color:green>{@value application.package_info#Version}</code>
+ * @author <code style=color:blue>{@value application.package_info#Author}</code>
+ */
+class package_info {
+    static final String Author = "Marcel";
+    static final String Version = "1.0.0-SNAPSHOT";
+}
