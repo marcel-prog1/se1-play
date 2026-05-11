4d4d59c4102d20ff5c32eee65b8bab6bdb4136cd
Marcel Steffen <m.steffen.98@web.de>
2026-05-11 18:15:45 +0200

    enable SE1 runtime application startup

--- src/main/application/Application.java
+++ src/main/application/Application.java
@@ -1,22 +1,89 @@
 package application;
 
-import java.util.Arrays;
+import runtimeSE.Runner;
+
+import runtimeSE.RuntimeSE;
+
+import runtimeSE.Runner.Accessors;
 
 /**
- * Application class with a {@code main()} - function that parses command line
- * arguments.
- *
+
+ * Class with {@code main()} method as entry point for the Java VM.
+
  * @version <code style=color:green>{@value application.package_info#Version}</code>
+
  * @author <code style=color:blue>{@value application.package_info#Author}</code>
+
  */
-public class Application {
+
+@Accessors(priority=0)
+
+public class Application implements Runner {
+
+    /**
+
+     * Method {@code main()} is the entry point for the <i>Java Virtual Machine (VM)</i>.
+
+     * @param args arguments passed from the command line.
+
+     */
+
     public static void main(String[] args) {
-        var module = Application.class.getModule().getName();
-        var greeting = String.format(module==null? "%s, se1-play" : "%s, %s (modular)", "Hello", module);
+
+        RuntimeSE.getInstance().startup(args);
+
+    }
+
+    /**
+
+     * Method invoked by the runtime. Application code starts here.
+
+     * @param runtime reference to the runtime singleton instance ({@link RuntimeSE}).
+
+     * @param args arguments passed from the command line.
+
+     */
+
+    @Override
+
+    public void run(RuntimeSE runtime, String[] args) {
+
+        String applicationName = runtime.properties().getProperty(
+
+            "application.name",
+
+            "unknown (no 'application.name' property)"
+
+        );
+
+        String applicationVersion = runtime.properties().getProperty(
+
+            "application.version",
+
+            "unknown version (no 'application.version' property)"
+
+        );
+
+        String greeting = String.format(
+
+            "Hello, %s (version %s)",
+
+            applicationName,
+
+            applicationVersion
+
+        );
+
         System.out.println(greeting);
 
-        Arrays.stream(args)
-            .map(arg -> String.format(" - arg: %s", arg))
-            .forEach(System.out::println);
+        for (String arg : args) {
+
+            String output = String.format(" - arg: %s", arg);
+
+            System.out.println(output);
+
+        }
+
     }
+
 }
